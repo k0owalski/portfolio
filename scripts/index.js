aff9bc58d7b337d224f97e8f24ef5623a71eff8b
kowalski <knrd.kowalik@gmail.com>
2022-12-12 16:34:20 +0100

    Added topbar and navbar

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,8 @@
+(function () {
+	const hamburger = document.querySelector('#hamburger');
+	const headerNav = document.querySelector('#header-nav');
 
+	hamburger.addEventListener('click', function () {
+		headerNav.classList.toggle('is-active');
+	});
+})();
