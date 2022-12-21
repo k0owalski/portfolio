e19567c2f178ef869b4aba5354385d26d77309bc
kowalski <knrd.kowalik@gmail.com>
2022-12-21 18:46:06 +0100

    Starting from the beginning

--- scripts/index.js
+++ scripts/index.js
@@ -1,8 +1,1 @@
-(function () {
-	const hamburger = document.querySelector('#hamburger');
-	const headerNav = document.querySelector('#header-nav');
-
-	hamburger.addEventListener('click', function () {
-		headerNav.classList.toggle('is-active');
-	});
-})();
+(function () {})();
