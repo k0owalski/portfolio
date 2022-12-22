96f0b9be01edbfec614cc01b3eac07c6c96a58be
kowalski <knrd.kowalik@gmail.com>
2022-12-22 01:29:38 +0100

    Added header and contact section

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,9 @@
-(function () {})();
+(function () {
+	const hamburger = document.querySelector('#hamburger');
+
+	hamburger.addEventListener('click', () => {
+		const header = document.querySelector('#header');
+
+		header.classList.toggle('is-active');
+	});
+})();
