dea993f0b088ed3c43c4a854e042b0652adffac2
kowalski <knrd.kowalik@gmail.com>
2022-12-23 19:19:21 +0100

    Added portfolio section

--- scripts/index.js
+++ scripts/index.js
@@ -2,6 +2,10 @@
 	const hamburger = document.querySelector('#hamburger');
 	const header = document.querySelector('#header');
 	const navItems = document.querySelectorAll('.nav-item-link');
+	const sliderControls = document.querySelectorAll(
+		'.portfolio .slider-controls .dot'
+	);
+	const sliderInner = document.querySelector('.portfolio .slider-inner');
 
 	hamburger.addEventListener('click', () =>
 		header.classList.toggle('is-active')
@@ -9,5 +13,23 @@
 
 	for (let item of navItems) {
 		item.addEventListener('click', () => header.classList.remove('is-active'));
+	}
+
+	for (let dot of sliderControls) {
+		dot.addEventListener('click', function (event) {
+			const sliderWidth = document.querySelector(
+				'.portfolio .slider .slide'
+			).clientWidth;
+
+			for (let d of sliderControls) {
+				d.classList.remove('is-active');
+			}
+
+			dot.classList.add('is-active');
+
+			sliderInner.style.translate = `calc((-${sliderWidth}px - 1rem) * ${parseInt(
+				event.target.dataset.slideId
+			)}) 0`;
+		});
 	}
 })();
